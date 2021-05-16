55da834fa46dea15ac5f310cd4aeaa65b41ce186
aguiot-- <aguiot--@student.42.fr>
2021-05-16 21:15:14 +0200

    wordpress: Add creation of 2 default users in entrypoint

--- srcs/images/wordpress/docker-entrypoint.sh
+++ srcs/images/wordpress/docker-entrypoint.sh
@@ -8,6 +8,10 @@ if wp-cli core is-installed; then
 else
 	echo "WordPress is not installed, installing..."
 	wp-cli core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_MAIL" --skip-email
+	echo "Creating user 'bob'..."
+	wp-cli user create bob bob@example.com --role=author
+	echo "Creating user 'alice'..."
+	wp-cli user create alice alice@example.com
 	if [ -n "$WP_THEME" ] && ! wp-cli theme is-active "$WP_THEME"; then
 		wp-cli theme activate "$WP_THEME"
 	fi
