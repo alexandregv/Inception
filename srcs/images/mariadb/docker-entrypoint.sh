e8036cf83c25f2d651c6b3efb12f43f4020f6171
aguiot-- <aguiot--@student.42.fr>
2021-05-17 01:41:28 +0200

    mariadb: Remove DB_ROOT_PASS, leaving only unix_socket authentication available

--- srcs/images/mariadb/docker-entrypoint.sh
+++ srcs/images/mariadb/docker-entrypoint.sh
@@ -13,20 +13,6 @@ if ! mariadb-admin --socket=/var/lib/mysql/mysql.sock --wait=25 ping; then
 	exit 1
 fi
 
-# Secure root account if not already done
-if [ ! -f /var/lib/mysql/.mariadb_secured ]; then
-	if [ -z "$DB_ROOT_PASS" ]; then
-		echo "DB_ROOT_PASS can't be blank, please provide a secure password."
-		exit 1
-	fi
-	mariadb --socket=/var/lib/mysql/mysql.sock -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASS');"
-	mariadb --socket=/var/lib/mysql/mysql.sock -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-	mariadb --socket=/var/lib/mysql/mysql.sock -e "DELETE FROM mysql.user WHERE User=''"
-	mariadb --socket=/var/lib/mysql/mysql.sock -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
-	mariadb --socket=/var/lib/mysql/mysql.sock -e "FLUSH PRIVILEGES"
-	touch /var/lib/mysql/.mariadb_secured
-fi
-
 # Import seed if database does not exist yet
 if ! (mariadb --socket=/var/lib/mysql/mysql.sock -e "SHOW DATABASES LIKE '$DB_NAME';" | grep -q "$DB_NAME"); then
 	echo "Initializing database..."
