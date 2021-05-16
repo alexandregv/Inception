2ff170ae016f2672dc7093a01ef7c5b08468cf54
aguiot-- <aguiot--@student.42.fr>
2021-05-16 21:03:20 +0200

    wordpress: Add WP_THEME to choose custom theme

--- srcs/images/wordpress/docker-entrypoint.sh
+++ srcs/images/wordpress/docker-entrypoint.sh
@@ -8,7 +8,9 @@ if wp-cli core is-installed; then
 else
 	echo "WordPress is not installed, installing..."
 	wp-cli core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_MAIL" --skip-email
-	wp-cli theme activate twentytwentyone
+	if [ -n "$WP_THEME" ] && ! wp-cli theme is-active "$WP_THEME"; then
+		wp-cli theme activate "$WP_THEME"
+	fi
 	echo "Installation done, starting..."
 fi
 
