6952421649a26967f792420508a5e2835877692d
aguiot-- <aguiot--@student.42.fr>
2021-05-16 03:25:08 +0200

    wordpress: Add dockerize

--- srcs/images/wordpress/docker-entrypoint.sh
+++ srcs/images/wordpress/docker-entrypoint.sh
@@ -4,8 +4,8 @@ if wp-cli core is-installed; then
 	echo "WordPress is already installed, starting..."
 else
 	echo "WordPress is not installed, installing..."
-	wp-cli core install --title='yay' --admin_user='wordpress' --admin_password='wordpress' --admin_email='wordpress@mail.com' --skip-email:w
+	wp-cli core install --title='yay' --admin_user='wordpress' --admin_password='wordpress' --admin_email='wordpress@mail.com' --skip-email
 	echo "Installation done, starting..."
 fi
 
-exec php-fpm7 -F -R
+exec dockerize -wait tcp://"$DB_HOST":3306 php-fpm7 -F -R
