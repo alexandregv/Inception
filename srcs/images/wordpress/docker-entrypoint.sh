7ebe0c561503a7c508f1d2fe16c6031990dbbad2
aguiot-- <aguiot--@student.42.fr>
2021-05-16 03:47:28 +0200

    wordpress: Wait for DB before wp-cli, select WP theme and fix missing --url

--- srcs/images/wordpress/docker-entrypoint.sh
+++ srcs/images/wordpress/docker-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+# wait for database
+dockerize -wait tcp://"$DB_HOST":3306
+
 if wp-cli core is-installed; then
 	echo "WordPress is already installed, starting..."
 else
 	echo "WordPress is not installed, installing..."
-	wp-cli core install --title='yay' --admin_user='wordpress' --admin_password='wordpress' --admin_email='wordpress@mail.com' --skip-email
+	wp-cli core install --url="localhost/wordpress" --title="aguiot--'s Inception" --admin_user="wordpress" --admin_password="wordpress" --admin_email="wordpress@mail.com" --skip-email
+	wp-cli theme activate twentytwentyone
 	echo "Installation done, starting..."
 fi
 
-exec dockerize -wait tcp://"$DB_HOST":3306 php-fpm7 -F -R
+# start php-fpm, becoming PID 1
+exec php-fpm7 -F -R
