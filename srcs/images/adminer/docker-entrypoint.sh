4f701f5efbdfe79c0d04916e1d108c285be02633
aguiot-- <aguiot--@student.42.fr>
2021-05-17 20:17:52 +0200

    adminer: Add dockerize to redirect stdout/stderr and configure php-fpm logfiles' accordingly

--- srcs/images/adminer/docker-entrypoint.sh
+++ srcs/images/adminer/docker-entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-# Start php-fpm, becoming PID 1
-exec php-fpm7 --nodaemonize
+# Make sure logfiles exist
+touch /var/log/php7/access.log /var/log/php7/error.log
+
+# Test FPM configuration and exit if invalid
+if ! php-fpm7 --test; then
+	echo "FPM configuration file is invalid, exiting."
+	exit 1
+fi
+
+# Start php-fpm via dockerize, becoming PID 1
+exec dockerize -stdout /var/log/php7/access.log -stderr /var/log/php7/error.log php-fpm7 --nodaemonize
