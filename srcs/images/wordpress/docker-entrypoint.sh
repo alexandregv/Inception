f801ed5a7bd50a215ef7e6f9cb645b59b53f7c06
aguiot-- <aguiot--@student.42.fr>
2021-05-17 20:19:42 +0200

    wordpress: Redirect stdout/stderr with dockerize and configure logfiles accordingly

--- srcs/images/wordpress/docker-entrypoint.sh
+++ srcs/images/wordpress/docker-entrypoint.sh
@@ -35,5 +35,14 @@ if [ -n "$WP_THEME" ] && ! wp-cli theme is-active "$WP_THEME"; then
 	wp-cli theme activate "$WP_THEME"
 fi
 
-# Start php-fpm, becoming PID 1
-exec php-fpm7 -F
+# Test FPM configuration and exit if invalid
+if ! php-fpm7 --test; then
+	echo "FPM configuration file is invalid, exiting."
+	exit 1
+fi
+
+# Make sure logfiles exist
+touch /var/log/php7/access.log /var/log/php7/error.log
+
+# Start php-fpm via dockerize, becoming PID 1
+exec dockerize -stdout /var/log/php7/access.log -stderr /var/log/php7/error.log php-fpm7 --nodaemonize
