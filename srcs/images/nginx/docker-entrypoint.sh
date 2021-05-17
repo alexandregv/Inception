2778912f090af714a8ed14383bb329f809082af1
aguiot-- <aguiot--@student.42.fr>
2021-05-17 03:56:51 +0200

    nginx: Enable SSL, disable port 80 and add volume for SSL certificate

--- srcs/images/nginx/docker-entrypoint.sh
+++ srcs/images/nginx/docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+if [ ! -f /etc/nginx/ssl/cert.crt ] || [ ! -f /etc/nginx/ssl/private.key ]; then
+	rm -f /etc/nginx/ssl/cert.crt /etc/nginx/ssl/private.key 
+	echo "Generating SSL certificate..."
+	openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout /etc/nginx/ssl/private.key -out /etc/nginx/ssl/cert.crt -subj "/CN=aguiot--.42.fr" -addext "subjectAltName=DNS:aguiot--.42.fr,DNS:aguiot--.42.fr"
+fi
+
 nginx -t -v
 nginx -g "daemon off;" $@
