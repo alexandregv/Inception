586966b23eeb0393e6167c33daa576cd068cb729
aguiot-- <aguiot--@student.42.fr>
2021-05-17 20:55:03 +0200

    adminer: Disable FPM config check on runtime, keep only build time

--- srcs/images/adminer/docker-entrypoint.sh
+++ srcs/images/adminer/docker-entrypoint.sh
@@ -4,10 +4,10 @@
 touch /var/log/php7/access.log /var/log/php7/error.log
 
 # Test FPM configuration and exit if invalid
-if ! php-fpm7 --test; then
-	echo "FPM configuration file is invalid, exiting."
-	exit 1
-fi
+#if ! php-fpm7 --test; then
+#       echo "FPM configuration file is invalid, exiting."
+#       exit 1
+#fi
 
 # Start php-fpm via dockerize, becoming PID 1
 exec dockerize -stdout /var/log/php7/access.log -stderr /var/log/php7/error.log php-fpm7 --nodaemonize
