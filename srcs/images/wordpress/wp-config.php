1aae06c51ec01795abc4f4bd0cbdb3246b919238
aguiot-- <aguiot--@student.42.fr>
2021-05-19 03:34:41 +0200

    Misc comments and formatting

--- srcs/images/wordpress/wp-config.php
+++ srcs/images/wordpress/wp-config.php
@@ -51,6 +51,7 @@ define( 'WP_REDIS_DATABASE', getenv('WP_REDIS_DATABASE') ?: 0 );
  * Change these to different unique phrases!
  * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
  * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ * Default seed: bG9sIGknbSBhIGNoZWF0ZXIsIEkgY29waWVkIHRoaXMgZnJvbSBhZ3Vpb3QtLQ==
  *
  * @since 2.6.0
  */
