79995f124592231907b558b830b854657a7d8275
aguiot-- <aguiot--@student.42.fr>
2021-05-20 17:08:11 +0200

    ministe: Render basic HTML for index

--- srcs/images/minisite/http/controller/pages/pages.go
+++ srcs/images/minisite/http/controller/pages/pages.go
@@ -8,5 +8,8 @@ import (
 
 // Called by the pages.index route (path: "/")
 func Index(response *goyave.Response, request *goyave.Request) {
-	response.String(http.StatusOK, "Hello, World!\nI made some presentation slides with the awesome <a href=\"https://sli.dev\">Slidev</a> tool, check them <a href=\"/slides\">here</a>.")
+	err := response.RenderHTML(http.StatusOK, "index.html", nil)
+	if  err != nil {
+		response.Error(err)
+	}
 }
